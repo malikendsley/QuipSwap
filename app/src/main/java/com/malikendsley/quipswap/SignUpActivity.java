5e3f562b86055bee99d58e4b0f8eb72f9513b6b2
Malik Endsley <malik.s.endsley@gmail.com>
2022-02-20 12:30:20 -0500

    desktop sync

--- app/src/main/java/com/malikendsley/quipswap/SignUpActivity.java
+++ app/src/main/java/com/malikendsley/quipswap/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,15 +26,17 @@ public class SignUpActivity extends AppCompatActivity {
         signupButton = findViewById(R.id.signUpButton);
 
 
-        signupButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String txt_email = email.getText().toString();
-                String txt_password = password.getText().toString();
-                //rudimentary validation, can use onTextChanged() later
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
-                    Toast.makeText(SignUpActivity.this, "Missing Information", Toast.LENGTH_SHORT).show();
-                }
+        signupButton.setOnClickListener(v -> {
+            String txt_email = email.getText().toString();
+            String txt_password = password.getText().toString();
+            //rudimentary validation, can use onTextChanged() later
+            if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                Toast.makeText(SignUpActivity.this, "Missing Information", Toast.LENGTH_SHORT).show();
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(txt_email).matches()){
+                Toast.makeText(SignUpActivity.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+            } else {
+                //register user
+                Toast.makeText(SignUpActivity.this, "Register User", Toast.LENGTH_LONG).show();
             }
         });
     }
