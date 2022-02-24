85a0e097280895d18157ccf5589d6c23ddee35cd
Malik Endsley <malik.s.endsley@gmail.com>
2022-02-24 01:51:45 -0500

    Implementing several useful Firebase database functions

---
+++ app/src/main/java/com/malikendsley/firebaseutils/FirebaseUtils.java
@@ -0,0 +1,17 @@
+package com.malikendsley.firebaseutils;
+
+
+//collection of functions intended to make interfacing with Firebase standardized and concise
+
+public class FirebaseUtils {
+    //TODO: Implement these, then convert them to cloud functions (need to learn how)
+    //Store a Quip
+    //Create a User
+    //Share a Quip
+    //Retrieve your Quips
+    //Retrieve shared Quips
+    //Retrieve Friends
+
+    //Load User
+    //Load Quip
+}
--- app/src/main/java/com/malikendsley/quipswap/MainActivity.java
+++ app/src/main/java/com/malikendsley/quipswap/MainActivity.java
@@ -1,5 +1,4 @@
 package com.malikendsley.quipswap;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
