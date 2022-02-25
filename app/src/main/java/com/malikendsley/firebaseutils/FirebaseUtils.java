abf8621616325bd224d3079ae2304c6a234969f9
Malik Endsley <malik.s.endsley@gmail.com>
2022-02-25 15:42:55 -0500

    FirebaseUtils scaffolding + Schema enforcement

--- app/src/main/java/com/malikendsley/firebaseutils/FirebaseUtils.java
+++ app/src/main/java/com/malikendsley/firebaseutils/FirebaseUtils.java
@@ -6,12 +6,43 @@ package com.malikendsley.firebaseutils;
 public class FirebaseUtils {
     //TODO: Implement these, then convert them to cloud functions (need to learn how)
     //Store a Quip
-    //Create a User
-    //Share a Quip
-    //Retrieve your Quips
-    //Retrieve shared Quips
-    //Retrieve Friends
+    /*
 
+
+     */
+    //Create a User
+    /*
+    Create user in authentication section
+    Grab the UID from that step and use it to create a full record in Users Collection
+     */
+    //Share a Quip
+    /*
+
+
+     */
+    //Retrieve your Quips
+    /*
+
+
+     */
+    //Retrieve shared Quips
+    /*
+
+
+     */
+    //Retrieve Friends
+    /*
+
+
+     */
     //Load User
+    /*
+
+
+     */
     //Load Quip
+    /*
+
+
+    */
 }
