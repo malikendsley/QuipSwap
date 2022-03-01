3db6eb863db281831db67fc2fbb829f2c46646a0
Malik Endsley <malik.s.endsley@gmail.com>
2022-03-01 17:03:47 -0500

    Fingerpaint prototype works, next is theme and integrate w firebase

--- app/src/main/java/com/malikendsley/quipswap/MakeQuipActivity.java
+++ app/src/main/java/com/malikendsley/quipswap/MakeQuipActivity.java
@@ -1,14 +1,57 @@
 package com.malikendsley.quipswap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.malikendsley.fingerpainting.PaintView;
 
 public class MakeQuipActivity extends AppCompatActivity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_quip);
+
+        paintView = (PaintView) findViewById(R.id.paintView);
+        //TODO Find out what this does exactly
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.paint_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
