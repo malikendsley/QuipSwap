553e152b53c493317e0743e24c834b7a07bd38de
Malik Endsley <malik.s.endsley@gmail.com>
2022-05-20 05:41:12 -0400

    gracefully handle creating a widget when not logged in (ask to log in)

--- app/src/main/java/com/malikendsley/quipswap/QuipWidgetConfigureActivity.java
+++ app/src/main/java/com/malikendsley/quipswap/QuipWidgetConfigureActivity.java
@@ -1,31 +1,36 @@
 package com.malikendsley.quipswap;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.malikendsley.firebaseutils.FirebaseHandler2;
 import com.malikendsley.firebaseutils.secureadapters.SecureFriendAdapter;
 import com.malikendsley.firebaseutils.secureinterfaces.FriendRetrieveListener;
 import com.malikendsley.quipswap.databinding.QuipWidgetConfigureBinding;
+import com.malikendsley.quipswap.navfragments.SignInFragment;
 
 import java.util.ArrayList;
 
 /**
  * The configuration screen for the {@link QuipWidget QuipWidget} AppWidget.
  */
-public class QuipWidgetConfigureActivity extends Activity {
+public class QuipWidgetConfigureActivity extends AppCompatActivity {
 
     private static final String TAG = "Own";
     private static final String PREFS_NAME = "com.malikendsley.quipswap.QuipWidget";
@@ -73,75 +78,87 @@ public class QuipWidgetConfigureActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
-        // Set the result to CANCELED.  This will cause the widget host to cancel
-        // out of the widget placement if the user presses the back button.
+        //with this, we will back out of the widget placement if the user presses the back button.
         setResult(RESULT_CANCELED);
 
+        //bind
         com.malikendsley.quipswap.databinding.QuipWidgetConfigureBinding binding = QuipWidgetConfigureBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //mAppWidgetText = binding.appwidgetText;
-        binding.addButton.setOnClickListener(view -> finish());
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Log.i(TAG, "QuipWidgetConfigure: User Not Logged In, Redirecting");
 
-        // Find the widget id from the intent.
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        if (extras != null) {
-            mAppWidgetId = extras.getInt(
-                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-        }
+            //make the empty frame visible and display a login fragment
+            binding.configureLoginContainer.setVisibility(View.VISIBLE);
+            Fragment mFragment;
+            mFragment = new SignInFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.configureLoginContainer, mFragment).commit();
 
-        // If this activity was started with an intent without an app widget ID, finish with an error.
-        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
-            finish();
-            return;
-        }
+        } else {
+            //make normal view visible and set up button
+            binding.sourceSelectionLayout.setVisibility(View.VISIBLE);
+            binding.addButton.setOnClickListener(view -> finish());
 
-        mdb2.getFriends(new FriendRetrieveListener() {
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onGetFriends(ArrayList<String> friendUIDList) {
-                if (friendUIDList != null) {
-                    friendList.clear();
-                    friendList.addAll(friendUIDList);
+            // Find the widget id from the intent.
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                mAppWidgetId = extras.getInt(
+                        AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            }
+
+            // If this activity was started with an intent without an app widget ID, finish with an error.
+            if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+                finish();
+                Log.i(TAG, "QuipConfigure: No appwidget ID");
+                //TODO This may occur because the user is not logged in, so consider blowing up a fragment in here
+                return;
+            }
+
+            mdb2.getFriends(new FriendRetrieveListener() {
+                @SuppressLint("NotifyDataSetChanged")
+                @Override
+                public void onGetFriends(ArrayList<String> friendUIDList) {
+                    if (friendUIDList != null) {
+                        friendList.clear();
+                        friendList.addAll(friendUIDList);
+                    }
+                    friendAdapter.notifyDataSetChanged();
                 }
-                friendAdapter.notifyDataSetChanged();
-            }
 
-            @Override
-            public void onGetFailed(Exception e) {
-                e.printStackTrace();
-                Toast.makeText(QuipWidgetConfigureActivity.this, "Having trouble connecting to the database", Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void onGetFailed(Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(QuipWidgetConfigureActivity.this, "Having trouble connecting to the database", Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        //set up recycler
-        initFriendRecycler();
+            //set up recycler
+            initFriendRecycler();
 
-        //when a friend is selected, store their UID in preferences for the QuipWidget.java class to use
-        friendAdapter = new SecureFriendAdapter(friendList, position -> {
+            //when a friend is selected, store their UID in preferences for the QuipWidget.java class to use
+            friendAdapter = new SecureFriendAdapter(friendList, position -> {
 
-            mFriendUID = friendList.get(position);
-            final Context context = QuipWidgetConfigureActivity.this;
-            // When the button is clicked, store the string locally
-            Log.i(TAG, "Row Clicked, storing " + mFriendUID + " in sharedPrefs");
-            saveFriendUIDPref(context, mAppWidgetId, mFriendUID);
-            // It is the responsibility of the configuration activity to update the app widget
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            QuipWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
-            // Make sure we pass back the original appWidgetId
-            Intent resultValue = new Intent();
-            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-            QuipWidgetConfigureActivity.this.setResult(RESULT_OK, resultValue);
-            QuipWidgetConfigureActivity.this.finish();
-        }, QuipWidgetConfigureActivity.this);
+                mFriendUID = friendList.get(position);
+                final Context context = QuipWidgetConfigureActivity.this;
+                // When the button is clicked, store the string locally
+                Log.i(TAG, "Row Clicked, storing " + mFriendUID + " in sharedPrefs");
+                saveFriendUIDPref(context, mAppWidgetId, mFriendUID);
+                // It is the responsibility of the configuration activity to update the app widget
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                QuipWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+                // Make sure we pass back the original appWidgetId
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                QuipWidgetConfigureActivity.this.setResult(RESULT_OK, resultValue);
+                QuipWidgetConfigureActivity.this.finish();
+            }, QuipWidgetConfigureActivity.this);
 
 
-        friendRecycler.setAdapter(friendAdapter);
+            friendRecycler.setAdapter(friendAdapter);
 
-
-        //mAppWidgetText.setText(loadTitlePref(QuipWidgetConfigureActivity.this, mAppWidgetId));
+        }
     }
 
     private void initFriendRecycler() {
